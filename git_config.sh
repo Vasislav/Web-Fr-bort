ab8beaf0384ef540f0a1153fe9d887b8803ab4a4
Vasislav <fryvasek@seznam.cz>
2024-09-24 07:45:36 +0200

    nove veci

--- git_config.sh
+++ git_config.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+git config --list
 git config --global user.name "Vasislav"
 git config --global user.email "fryvasek@seznam.cz"
 echo "Přihlášení uživatele proběhlo v pořádku"
-#cd eshop_colorbios
+composer create-project laravel/laravel example-app
+# cd eshop_colorbios
+# php artisan serve
+# fontshare
